13048c3d5f19df97fb374d8f1c672018c0171bbd
Ragothaman-Ramesh <152747186+Ragothaman-Ramesh@users.noreply.github.com>
2024-02-05 09:30:21 -0800

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,25 +5,25 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Product Consultant",
+    cardImage: "assets/images/experience-page/SCEIN-LOGO-COUNTIES-F-e1547837169969.png",
+    place: "Cleantech, San Diego",
+    time: "(Sep, 2023 - present)",
+    desp: "<li>Developed Go-to-Market strategies for four San Diego-based renewable energy startups, driving a projected 25% increase in potential revenue and an enhanced product-market fit.</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Section Lead",
+    cardImage: "assets/images/experience-page/Titan_Company_Logo.png",
+    place: "Titan Company (TATA Group), Bengaluru, India",
+    time: "(Apr - Jun, 2022)",
+    desp: "<li>Devised a Go-To-Market (GTM) Strategy for an international business expansion in under six months resulting in a projected 10% market share and potential annual revenue of $5 million.</li><li>Supported the creation of three performance dashboards, enabling real-time monitoring and achieving a 13% average reduction in project timelines.</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Senior Engineer (New Products)",
+    cardImage: "assets/images/experience-page/Titan_Company_Logo.png",
+    place: "Titan Company (TATA Group), Hosur, India)",
+    time: "(Jul, 18 - Mar, 22)",
+    desp: "<li>Led end-to-end product development for 30 new products from product design to product delivery, ensuring strategy alignment, costing, and 95% on-time delivery.</li><li>Spearheaded a cost-saving initiative involving cross-functional teams, leading to a 38% reduction in time-to-market.</li></li>Executed a technical problem-solving project through the Lean Six Sigma method, yielding a 30% reduction in market returns.</li>",
   },
 ];
 
